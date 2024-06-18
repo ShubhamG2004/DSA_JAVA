328f6430ca2180723aded237f1362baada501095
shubham <gavadeshubham2004@gmail.com>
2024-06-18 11:09:35 +0530

    modified:   Stack_imp.java

--- Stack_imp.java
+++ Stack_imp.java
@@ -1,9 +1,28 @@
-import java.util.Stack;
-
+import java.util.*;
 public class Stack_imp {
     public static void main(String[] args) {
         Stack<Integer> st=new Stack<>();
-        st.push(10);
-        System.out.println(st.peek());
+        Stack<Integer> rt=new Stack<>();
+        Stack<Integer> gt=new Stack<>();
+        // st.push(10);
+        // System.out.println(st.peek());
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter N:");
+        int n=sc.nextInt();
+
+        System.out.println("Enter Element:");
+        for(int i=1;i<=n;i++){
+            int a=sc.nextInt();
+            st.push(a);
+        }
+        System.out.println(st);
+
+        while(st.size()>0){
+            rt.push(st.pop());  
+        }
+        while(rt.size()>0){
+            gt.push(rt.pop());  
+        }
+        System.out.println("ft:"+gt);
     }
 }
