949b37b0aaa0ff61502a3efb9b5cc4c467f56307
shubham <gavadeshubham2004@gmail.com>
2024-06-27 20:01:46 +0530

    modified:   .vscode/launch.json 	modified:   InfixToPrefix.java

--- InfixToPrefix.java
+++ InfixToPrefix.java
@@ -2,8 +2,66 @@ import java.util.Stack;
 
 public class InfixToPrefix {
     public static void main(String[] args) {
-        String infix=""
-        Stack<String> val=new Stack<>();
-        Stack<Character> op=new Stack<>();
+        String infix = "9-(5+3)*4/6";
+        System.out.println("Infix: " + infix);
+        System.out.println("Prefix: " + infixToPrefix(infix));
+    }
+
+    public static String infixToPrefix(String infix) {
+        Stack<String> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for (int i = infix.length() - 1; i >= 0; i--) {
+            char ch = infix.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                val.push(ch + "");
+            } else if (ch == ')') {
+                op.push(ch);
+            } else if (ch == '(') {
+                while (op.peek() != ')') {
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char o = op.pop();
+                    String t = o + v1 + v2;
+                    val.push(t);
+                }
+                op.pop();
+            } else {
+                while (!op.isEmpty() && precedence(op.peek()) > precedence(ch)) {
+                    String v2 = val.pop();
+                    String v1 = val.pop();
+                    char o = op.pop();
+                    String t = o + v1 + v2;
+                    val.push(t);
+                }
+                op.push(ch);
+            }
+        }
+
+        while (!op.isEmpty()) {
+            String v2 = val.pop();
+            String v1 = val.pop();
+            char o = op.pop();
+            String t = o + v1 + v2;
+            val.push(t);
+        }
+
+        return val.pop();
+    }
+
+    public static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case ')':
+                return 0; 
+            default:
+                return -1;
+        }
     }
 }
