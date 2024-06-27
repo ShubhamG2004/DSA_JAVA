e173dc4392a5d22c670a39b3fbf5633a5b6b78e5
shubham <gavadeshubham2004@gmail.com>
2024-06-27 14:25:39 +0530

    modified:   .vscode/launch.json 	new file:   InfixToPrefix.java

---
+++ InfixToPrefix.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+
+public class InfixToPrefix {
+    public static void main(String[] args) {
+        String infix=""
+        Stack<String> val=new Stack<>();
+        Stack<Character> op=new Stack<>();
+    }
+}
