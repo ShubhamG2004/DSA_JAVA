1739ded752695eab559f2d66351bd6e8fd908a1f
shubham <gavadeshubham2004@gmail.com>
2024-07-06 22:09:14 +0530

    modified:   Queue.java

--- Queue.java
+++ Queue.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+import java.util.*;
 public class Queue {
-    
+    // Displaying the queue element using extra Queue
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList();
+    }
 }
