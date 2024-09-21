825b4e1685a769d4e8505ea6d5b47b3a894c2d19
shubham <gavadeshubham2004@gmail.com>
2024-09-21 19:10:39 +0530

    modified:   Backtrancking1.java

---
+++ Backtrancking1.java
@@ -0,0 +1,15 @@
+class Backtrancking{
+    public static int RateInMaze(int sr,int sc,int er,int ec){
+        int rightways = RateInMaze(1, sc+1, er, ec);
+        int downways = RateInMaze(sr+1, sc, er, ec);
+        int totalways = rightways + downways;
+        return totalways;
+    }
+    public static void main(String[] args) {
+        int row=2;
+        int column=2;
+
+        int count = RateInMaze(1,1,row,column);
+        System.out.println(count);
+    }
+}
