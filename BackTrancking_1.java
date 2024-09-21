da606d48eaaa13812624b8b1e3a6810b68e3b923
shubham <gavadeshubham2004@gmail.com>
2024-09-21 19:19:09 +0530

    modified:   .vscode/launch.json 	modified:   BackTrancking_1.java

--- BackTrancking_1.java
+++ BackTrancking_1.java
@@ -1,13 +1,15 @@
 class BackTrancking_1{
     public static int RateInMaze(int sr,int sc,int er,int ec){
+        if(sr>er || sc > ec )return 0;
+        if(sr ==er && sc== ec )return 1;
         int rightways = RateInMaze(1, sc+1, er, ec);
         int downways = RateInMaze(sr+1, sc, er, ec);
         int totalways = rightways + downways;
         return totalways;
     }
     public static void main(String[] args) {
-        int row=2;
-        int column=2;
+        int row=6;
+        int column=6;
 
         int count = RateInMaze(1,1,row,column);
         System.out.println(count);
