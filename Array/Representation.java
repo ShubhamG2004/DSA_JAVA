392ce46cb314dbe757975cbfd71a4defb0f44d37
shubham <gavadeshubham2004@gmail.com>
2025-03-07 17:23:39 +0530

    modified:   .vscode/launch.json 	new file:   Array/Representation.java

---
+++ Array/Representation.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Representation {
+
+    static void SearchKey(int[][] Matrix, int Key) {
+        int m = Matrix.length;
+        int n = Matrix[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (Matrix[i][j] == Key) {
+                    System.out.println("Key found at: (" + i + ", " + j + ")");
+                }
+            }
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int m = 3;
+        int n = 3;
+
+        int[][] Matrix = new int[m][n];
+
+        System.out.println("Enter the Element:");
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                Matrix[i][j] = sc.nextInt();
+                // sc.close();
+            }
+        }
+
+        System.out.println("Element:");
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(Matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        SearchKey(Matrix, 8);
+    }
+}
