b5505bfb08082a12789e9fe171b6309ef951ea4f
shubham <gavadeshubham2004@gmail.com>
2025-04-20 11:43:27 +0530

    modified:   .vscode/launch.json 	new file:   BST/Construct.java

---
+++ BST/Construct.java
@@ -0,0 +1,68 @@
+public class Construct {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Construct BST without allowing duplicate values
+    public static TreeNode constructBST(TreeNode root, int data) {
+        if (root == null) {
+            return new TreeNode(data);
+        }
+
+        TreeNode temp = root;
+        while (true) {
+            if (data < temp.val) {
+                if (temp.left == null) {
+                    temp.left = new TreeNode(data);
+                    break;
+                } else {
+                    temp = temp.left;
+                }
+            } else if (data > temp.val) {
+                if (temp.right == null) {
+                    temp.right = new TreeNode(data);
+                    break;
+                } else {
+                    temp = temp.right;
+                }
+            } else {
+                // Duplicate value found, do not insert
+                break;
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {20, 8, 2, 6, 12, 19,4, 7};
+
+        TreeNode root = null;
+
+        for (int value : arr) {
+            root = constructBST(root, value);
+        }
+
+        System.out.println();
+        
+        printPreOrder(root);
+    }
+
+    
+    public static void printPreOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        printPreOrder(root.left);
+        printPreOrder(root.right);
+    }
+}
