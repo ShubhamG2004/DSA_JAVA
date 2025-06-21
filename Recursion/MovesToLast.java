8af74ef825f3c5d55369d5df989a137fa8cf6bd3
shubham <gavadeshubham2004@gmail.com>
2025-06-21 12:19:53 +0530

    modified:   .vscode/launch.json 	new file:   Recursion/MovesToLast.java

---
+++ Recursion/MovesToLast.java
@@ -0,0 +1,27 @@
+public class MovesToLast {
+    public static void main(String[] args) {
+        String str = "xxxabcbabc";
+        System.out.println("Original String: " + str);
+        StringBuilder newStr = new StringBuilder();
+        String result = moveAllXToEnd(str, 0, 0, newStr);
+        System.out.println("Modified String: " + result);
+    }
+    static String moveAllXToEnd(String str, int idx, int count, StringBuilder newStr) {
+
+        if(idx == str.length()){
+            for(int i=0;i<count;i++){
+                newStr.append('x');
+            }
+            return newStr.toString();
+        }
+        
+        char currChar = str.charAt(idx);
+        if( currChar == 'x'){
+            count++;
+            return moveAllXToEnd(str, idx+1, count, newStr);
+        }else{
+            newStr.append(currChar);
+            return moveAllXToEnd(str, idx+1, count, newStr);
+        }
+    }
+}
