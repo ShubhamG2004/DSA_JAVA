9a4c9ec2f3ae172817006e3d8f48c689cc30b746
shubham <gavadeshubham2004@gmail.com>
2025-06-21 12:37:22 +0530

    modified:   .vscode/launch.json 	new file:   Recursion/RemoveDuplicate.java

---
+++ Recursion/RemoveDuplicate.java
@@ -0,0 +1,23 @@
+public class RemoveDuplicate {
+    
+    public static void main(String[] args) {
+
+        String str = "aabbccddeeffgg";
+        StringBuilder newStr = new StringBuilder();
+        removeDuplicates(str, 0, newStr);
+        System.out.println("Original String: " + str);
+        System.out.println("Modified String: " + newStr.toString());
+    }
+    static void removeDuplicates(String str, int idx, StringBuilder newStr) {
+        if(idx == str.length()){
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(newStr.length()==0 || newStr.charAt(newStr.length()-1) != currChar){
+            newStr.append(currChar);
+            removeDuplicates(str, idx + 1, newStr);
+        } else {
+            removeDuplicates(str, idx+1, newStr);
+        }
+    }
+}
