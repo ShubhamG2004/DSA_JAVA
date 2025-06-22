891722e3cf748c08bc80ced214977af9983e772f
shubham <gavadeshubham2004@gmail.com>
2025-06-22 10:36:40 +0530

    modified:   .vscode/launch.json 	new file:   Recursion/KeyBoardPrint.java 	modified:   Recursion/printSubstring.java

---
+++ Recursion/KeyBoardPrint.java
@@ -0,0 +1,22 @@
+public class KeyBoardPrint {
+    public static void main(String[] args) {
+        String str = "234";
+        printKeyBoard(str, 0, "");
+    }
+    static String[] keyBoard = {
+        ".", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz","yz"
+    };
+    static void printKeyBoard(String str, int idx, String curr) {
+        if(str.length() == idx) {
+            System.out.println(curr);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        String mapp = keyBoard[currChar - '0'];
+
+        for(int i=0;i < mapp.length();i++){
+            printKeyBoard(str, idx+1, curr + mapp.charAt(i));
+        }
+    }
+}
--- Recursion/printSubstring.java
+++ Recursion/printSubstring.java
@@ -1,7 +1,10 @@
+import java.util.HashSet;
+
 public class printSubstring {
     public static void main(String[] args) {
-        String str = "abc";
+        String str = "aaa";
         printSubstrings(str, 0, "");
+        uniqueSubstrings(str, 0, "", new HashSet<>());
     }
     static void printSubstrings(String str, int idx, String curr){
         if(idx == str.length()){
@@ -11,5 +14,18 @@ public class printSubstring {
         char currChar = str.charAt(idx);
         printSubstrings(str, idx+1, curr + currChar);
         printSubstrings(str, idx+1, curr);
+    }
+    static void uniqueSubstrings(String str, int idx, String curr, HashSet<String> set){
+        if(idx == str.length()){
+            if(!set.contains(curr)){
+                System.out.println(curr);
+                set.add(curr);
+            }
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        uniqueSubstrings(str, idx+1, curr + currChar, set);
+        uniqueSubstrings(str, idx+1, curr, set);   
     }
 }
