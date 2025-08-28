f41f0b0a9b8cc378588cc4e81fac7cac8dac0b46
shubham <gavadeshubham2004@gmail.com>
2025-08-28 13:03:04 +0530

    IMplemented Reversed String Program

---
+++ Cognizant2/ReverseStringQues.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ReverseStringQues{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a String:");
+        String input = sc.nextLine();
+
+        String reverseString = new StringBuilder(input).reverse().toString();
+
+        System.out.println("Reversed String: \"" + reverseString + "\"");
+    }
+}
