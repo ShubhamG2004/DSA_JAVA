4ce2c26bd62f77de2860798c8f9b506965dff478
shubham <gavadeshubham2004@gmail.com>
2026-04-18 23:25:31 +0530

    basic problem solved

---
+++ PGP/Week1/Question1.java
@@ -0,0 +1,11 @@
+class Question1{
+    static int addition(int a, int b) {
+        // code here
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        int result = addition(5, 7);
+        System.out.println("Sum: " + result);
+    }
+}
