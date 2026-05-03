f61a5011051fa52003856bf027ee8431f0099598
shubham <gavadeshubham2004@gmail.com>
2026-05-03 23:03:40 +0530

    Added question8

--- PGP/Week3/Question8.java
+++ PGP/Week3/Question8.java
@@ -1,22 +1,21 @@
 package PGP.Week3;
 
+import java.util.Map;
+
 public class Question8 {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            count[c - 'a']--;
         }
 
-        int[] freq = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
-        }
-
-        for (int count : freq) {
-            if (count != 0) {
-                return false;
-            }
+        for (int val : count) {
+            if (val != 0) return false;
         }
 
         return true;
