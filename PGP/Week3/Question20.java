8995db56d934dc1bbc1e641e58db9ad0c22800b0
shubham <gavadeshubham2004@gmail.com>
2026-06-02 21:42:20 +0530

    Access memory

--- PGP/Week3/Question20.java
+++ PGP/Week3/Question20.java
@@ -1,5 +1,7 @@
 package PGP.Week3;
 
+import java.util.Arrays;
+
 public class Question20 {
     public int maxProduct(int[] nums) {
         int prefix = 1;
@@ -20,8 +22,30 @@ public class Question20 {
     }
 
     public static void main(String[] args) {
-        Question20 q19 = new Question20();
-        int[] nums = {2, 3, -2, 4};
-        System.out.println("Maximum product subarray: " + q19.maxProduct(nums));
+        Question20 q = new Question20();
+
+        int[][] tests = {
+                {2, 3, -2, 4},
+                {-2, 0, -1},
+                {-2},
+                {0, 2},
+                {-2, 3, -4},
+                {-1, -2, -3, 0},
+                {-2, -3, -4},
+                {0, 0, 0},
+                {-2, 0, -1, -3, 4}
+        };
+
+        int[] expected = {6, 0, -2, 2, 24, 6, 12, 0, 12};
+
+        for (int i = 0; i < tests.length; i++) {
+            runTest(q, tests[i], expected[i]);
+        }
+    }
+
+    private static void runTest(Question20 solver, int[] nums, int expected) {
+        int actual = solver.maxProduct(nums);
+        String status = (actual == expected) ? "PASS" : "FAIL";
+        System.out.printf("Input: %s | Expected: %d | Actual: %d | %s%n", Arrays.toString(nums), expected, actual, status);
     }
 }
