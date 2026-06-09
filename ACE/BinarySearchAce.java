2d3ed137e60203a13d7976d24f4f7f5c2d9f36d3
shubham <gavadeshubham2004@gmail.com>
2026-06-09 23:09:33 +0530

    Binary Seach and Bubble sort

---
+++ ACE/BinarySearchAce.java
@@ -0,0 +1,34 @@
+class BinarySearchAce {
+    static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 4, 15, 26, 37, 48, 59};
+        int target = 26;
+
+        int index = binarySearch(arr, target);
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
